bfb23fa8071b7ccfa4194f239f0fda5ebb169d3f
zlhshn <zsahin4401@gmail.com>
2023-12-03 13:51:58 +0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-x-twitter"></i>
+    </footer>
   )
 }
 
 export default Footer
+
